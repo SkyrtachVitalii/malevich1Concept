a05c5178b19eab11d6f2445b41896a223dd3a394
Skyrtach Vitalii <vitaliiskirtac@gmail.com>
2024-03-13 21:01:14 +0200

    menu

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ async function createDishesList(typesLink, listLink) {
 
 function createDishContent(dishesList, dishesTypes){
   let dishContent = document.querySelector('.dishes');
-  createDishHeaderAndBody(dishContent, dishesList, dishesTypes);
+    createDishHeaderAndBody(dishContent, dishesList, dishesTypes);
 }
 
 function createDishHeaderAndBody(dishContent, dishesList, dishesTypes){
@@ -174,8 +174,7 @@ setTimeout(() => {
 }, "300");
 
 
-
-
+// =========== кнопка для переміщення вгору =============
 const btnUp = {
   el: document.querySelector('.btn-up'),
   show() {
@@ -207,16 +206,56 @@ const btnUp = {
 }
 
 btnUp.addEventListener();
+// =======================================================
 
 
+let isMobile = {
+  Android: function () {
+      return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+      return (
+              isMobile.Android()
+              || isMobile.BlackBerry()
+              || isMobile.iOS()
+              || isMobile.Opera()
+              || isMobile.Windows()
+              );
+  }
+};
 
 
+let body = document.querySelector('body');
+if(isMobile.any()){
+  body.classList.add('touch');
+  let arrow = document.querySelectorAll(".arrow");
+  for (let i = 0; i < arrow.length; i++){
+    let thisLink = arrow[i].previousElementSibling;
+    let dropdownMenu = arrow[i].nextElementSibling;
+    let thisArrow = arrow[i];
 
+    thisLink.classList.add('parent');
 
-
-
-
-
+    arrow[i].addEventListener("click", function(){
+      dropdownMenu.classList.toggle('open');
+      thisArrow.classList.toggle('active');
+    })
+  }
+}else{
+  body.classList.add('mouse');
+}
 
 
 
